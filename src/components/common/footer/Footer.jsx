41718354208825f13dd1a7b4ad3e81287c96054d
jfmonsa <juanmonsalve@protonmail.com>
2024-04-19 16:07:32 -0500

    base html and footer icons

--- src/components/common/footer/Footer.jsx
+++ src/components/common/footer/Footer.jsx
@@ -1,6 +1,40 @@
 function Footer(){
     return (
-        <footer><p>Todos los derechos reservados xd</p></footer>
+        <footer>
+            <p>Todos los derechos reservados xd</p>
+            <div>
+                <h2>Atlas Books</h2>
+                <p>Disclaimer: Proyecto basado en z-library<a href=""></a></p>
+                <div>
+                    
+                </div>
+            </div>
+
+            <div>
+                <h2>Topic</h2>
+                <ul>
+                    <li>Page 1</li>
+                    <li>Page 2</li>
+                    <li>Page 3</li>
+                </ul>
+            </div>
+            <div>
+                <h2>Topic</h2>
+                <ul>
+                    <li>Page 1</li>
+                    <li>Page 2</li>
+                    <li>Page 3</li>
+                </ul>
+            </div>
+            <div>
+                <h2>Topic</h2>
+                <ul>
+                    <li>Page 1</li>
+                    <li>Page 2</li>
+                    <li>Page 3</li>
+                </ul>
+            </div>
+        </footer>
     );
 }
 
