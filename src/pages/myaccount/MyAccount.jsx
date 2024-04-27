f9390de820e1325bdfdf60afbb51c7dc9582feec
jfmonsa <juanmonsalve@protonmail.com>
2024-04-27 08:13:29 -0500

    Creating myData section-card in myAccountPage

--- src/pages/myaccount/MyAccount.jsx
+++ src/pages/myaccount/MyAccount.jsx
@@ -1,9 +1,41 @@
+import "./myaccount.css";
 import Card from "../../components/card/Card.jsx";
 
+// Aux functions
+const MyAccountMyData = () => {
+  return (
+    <Card h1_text="Mis datos" h1_center={false}>
+      <ul>
+        <li className="card-myData">
+          <span className="card-myData__left">Nombre</span>
+          <span className="card-myData__right">Pepito Perez</span>
+        </li>
+        <li className="card-myData">
+          <span className="card-myData__left">Email</span>
+          <span className="card-myData__right">pepito@p.com</span>
+        </li>
+        <li className="card-myData">
+          <span className="card-myData__left">País</span>
+          <span className="card-myData__right">Palestina</span>
+        </li>
+        <li className="card-myData">
+          <span className="card-myData__left">Fecha de Registro</span>
+          <span className="card-myData__right">11/09/2001</span>
+        </li>
+      </ul>
+    </Card>
+  );
+};
+
+const MyAccountOtherOpts = () => {
+  return <Card></Card>;
+};
+
+// Main page
 const MyAccount = () => {
   return (
     <>
-      <Card h1_text="Mis datos" h1_center={false}></Card>;
+      <MyAccountMyData />
     </>
   );
 };
