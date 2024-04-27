4110eee44ea254de5b93ad8653f2f4ac52b9eb2c
Juan Sebastian Marin Serna <sebasagosto2003@gmail.com>
2024-04-26 21:15:21 -0500

    Botones desplegables terminados detalles por terminar

---
+++ src/components/dropDownButtons/dropDownDownload.jsx
@@ -0,0 +1,58 @@
+import "./dropDownButton.sass"
+import Mega from "../../assets/icons/Icon-mega.svg"
+import Mediafire from "../../assets/icons/Icon-mediafire.svg"
+import { FaChevronLeft } from 'react-icons/fa';
+import { FaChevronDown } from 'react-icons/fa';
+import {useState} from "react";
+import {Link} from "react-router-dom";
+
+const DropDownButtonDescarga = () => {
+
+
+    const DropdownItemDescarga = props => {
+        const alticon = "Icono de";
+        return (
+          <li>
+            <Link to={props.toLink} className="dropmenu__itemBook">
+              <span className="dropmenu__item__iconBook">
+                <img src={props.icon} alt={alticon + " " + props.text}></img>
+              </span>
+              <span className="dropmenu__item__textBook">{props.text}</span>
+            </Link>
+          </li>
+        );
+      };
+
+      const DropdownMenuDescarga = () => {
+        return (
+          <ul className="dropmenuBook">
+            <DropdownItemDescarga
+              toLink=""
+              icon={Mega}
+              text="Mega"
+            ></DropdownItemDescarga>
+            <DropdownItemDescarga
+              toLink=""
+              icon={Mediafire}
+              text="Mediafire"
+            ></DropdownItemDescarga>
+          </ul>
+        );
+      };
+
+    const [open, setOpen] = useState(false);
+
+    return (
+        <section className="download-dropdown">
+            <div className="icon-buttonBookDownload" onClick={() => setOpen(!open)}>
+                Descargar
+                <div className="divide-button"></div> 
+                <div className="icon-chevron">{open ? <FaChevronDown/> : <FaChevronLeft/> }</div>
+            </div>
+
+             {open && <DropdownMenuDescarga />} 
+        </section>
+    )
+}
+
+export default DropDownButtonDescarga
--- src/pages/bookInformation/BookInformation.jsx
+++ src/pages/bookInformation/BookInformation.jsx
@@ -1,56 +1,19 @@
 import "./BookInformation.sass"
-import MenuProfileIcon from "../../assets/icons/menu-profile.svg";
-import MenuSearchIcon from "../../assets/icons/menu-search.svg";
-import { FaChevronLeft } from 'react-icons/fa';
-import { FaChevronDown } from 'react-icons/fa';
-import {useState} from "react";
-import {Link} from "react-router-dom";
+import DropDownButtonCompartir from "../../components/dropDownButtons/dropDownShare"
+import DropDownButtonDescarga from "../../components/dropDownButtons/dropDownDownload"
+import DropDownButtonListUser from "../../components/dropDownButtons/dropDownListUser"
 
 const BookInformation = () => {
-
-
-    const DropdownItem = props => {
-        const alticon = "Icono de";
-        return (
-          <li>
-            <Link to={props.toLink} className="dropmenu__itemLibro">
-              <span className="dropmenu__item__iconLibro">
-                <img src={props.icon} alt={alticon + " " + props.text}></img>
-              </span>
-              <span className="dropmenu__item__textLibro">{props.text}</span>
-            </Link>
-          </li>
-        );
-      };
-
-      const DropdownMenu = () => {
-        return (
-          <ul className="dropmenuLibro">
-            <DropdownItem
-              toLink=""
-              icon={MenuProfileIcon}
-              text="Mega"
-            ></DropdownItem>
-            <DropdownItem
-              toLink=""
-              icon={MenuSearchIcon}
-              text="Mediafire"
-            ></DropdownItem>
-          </ul>
-        );
-      };
-
-    const [open, setOpen] = useState(false);
-
-    return (
-        <section>
-            <div className="icon-buttonLibro" onClick={() => setOpen(!open)}>
-                Descargar <div className="pepe"></div> <div className="icon-flecha">{open ? <FaChevronDown/> : <FaChevronLeft/> }</div>
-            </div>
-
-            {open && <DropdownMenu />}
-        </section>
-    )
+  return (
+    <section>
+      <div>BookInformation</div>
+      <div className="Buttons">
+        <DropDownButtonDescarga/>
+        <DropDownButtonCompartir/>
+        <DropDownButtonListUser/>
+      </div>
+    </section>
+  )
 }
 
 export default BookInformation
