adc56a2b1dce9af0e2d59c9acba1f3ad7b784925
LeiderCortes <leidersantiago27@gmail.com>
2024-04-28 16:05:28 -0500

    button list report

--- src/pages/account/Report.jsx
+++ src/pages/account/Report.jsx
@@ -2,11 +2,13 @@ import Card from "../../components/card/Card.jsx";
 import PrimaryBtnForm from "../../components/buttons/primaryBtn/PrimaryBtnForm.jsx";
 import {Link} from "react-router-dom";
 import InputText from "../../components/inputText/InputText.jsx";
+// import DropDownButtonReport from "../../components/dropDownButtons/dropDownReport.jsx";
 //import DropDownButtonReport from "../../components/dropDownButtons/dropDownReport.jsx";
 
 const Report = () => {
   return (
     <Card cardDialog h1_text="Reportar">
+      {/* <DropDownButtonReport /> */}
       <form>
         <InputText holder="Contexto reporte" type={"text"} text="Reporte:" />
         <Link to="/book-information">
