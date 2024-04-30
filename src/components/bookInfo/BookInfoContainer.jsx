cccee51991eb03356f374e8ef8c70752b79a35a6
jfmonsa <juanmonsalve@protonmail.com>
2024-04-30 13:36:30 -0500

    fixing some sematic html errors in BookInfoContainer

--- src/components/bookInfo/BookInfoContainer.jsx
+++ src/components/bookInfo/BookInfoContainer.jsx
@@ -1,10 +1,10 @@
+import "./BookInfoContainer.css";
 import BookImage from "../../assets/img/image1.png";
 import DropDownButtonCompartir from "../dropDownButtons/dropDownShare";
 import DropDownButtonDescarga from "../dropDownButtons/dropDownDownload";
 import DropDownButtonListUser from "../dropDownButtons/dropDownListUser";
 import {Link} from "react-router-dom";
 import PrimaryBtnForm from "../buttons/primaryBtn/PrimaryBtnForm";
-import "./BookInfoContainer.css";
 import Star from "../../assets/icons/Star.svg";
 import coment from "../../assets/icons/comentario-icon.svg";
 import heart from "../../assets/icons/corazon-icon.svg";
@@ -25,78 +25,70 @@ export function BookInfoContainer({
 }) {
   return (
     <div className="book-container-info">
-      <div className="top-container-info">
-        <h1>Inicio / Libro</h1>
-      </div>
+      <h1 className="top-container-info">Inicio / libro</h1>
       <div className="book-info-content">
-        <div className="book-image">
-          <img src={BookImage} alt="bookImage" />
-        </div>
+        <img src={BookImage} alt="bookImage book-image" />
         <div className="book-container-info-rigth-part">
           <div>
             <h2 className="book-name">{bookName}</h2>
             <h3 className="book-author-name">{authorName}</h3>
             <div className="book-more-info">
-              <div>
-                <img src={Star} alt="star-book" className="rank-star" />
-              </div>
-              <div>{rank}/ 5.0</div>
-              <div>
-                <img src={coment} alt="coment-book" className="more-images" />
-              </div>
-              <div> 2 comentarios</div>
+              <img src={Star} alt="star-book" className="rank-star" />
+              <p>{rank}/ 5.0</p>
+              <img src={coment} alt="coment-book" className="more-images" />
+              <p> 2 comentarios</p>
               <button className="more-buttons">
                 <img src={heart} alt="heart" />
               </button>
               <button className="more-buttons">
                 <img src={mark} alt="" />
               </button>
-                <DropDownButtonListUser />
+              <DropDownButtonListUser />
             </div>
             <h3>Sinopsis</h3>
-            <h4 className="sinopsis">
+            <p>
               Thousands of miles away from the small township of 'Salem's Lot,
               two terrified people, a man and a boy, still share the secrets of
               those clapboard houses and tree-lined streets. They must return to
               'Salem's Lot for a final confrontation with the unspeakable evil
               that lives on in the town.
-            </h4>
+            </p>
             <div className="book-specs">
               <div className="specs">
-                <div className="column">
-                  <p>Categorias:</p>
-                  <p>Publicador:</p>
-                  <p>ISBN:</p>
-                  <p>Archivo:</p>
-                </div>
-                <div className="column">
-                  <p>{categories}</p>
-                  <p>{editory}</p>
-                  <p>{isbn}</p>
-                  <p>{fileType}</p>
-                </div>
+                <ul className="column">
+                  <li>Categorias:</li>
+                  <li>Publicador:</li>
+                  <li>ISBN:</li>
+                  <li>Archivo:</li>
+                </ul>
+                <ul className="column">
+                  <li>{categories}</li>
+                  <li>{editory}</li>
+                  <li>{isbn}</li>
+                  <li>{fileType}</li>
+                </ul>
               </div>
               <div className="specs">
-                <div className="column">
-                  <p>Año:</p>
-                  <p>Lenguaje:</p>
-                  <p>Paginas:</p>
-                  <p>Volumen:</p>
-                </div>
-                <dir className="column">
-                  <p>{year}</p>
-                  <p>{languaje}</p>
-                  <p>{pages}</p>
-                  <p>{vol}</p>
-                </dir>
+                <ul className="column">
+                  <li>Año:</li>
+                  <li>Lenguaje:</li>
+                  <li>Paginas:</li>
+                  <li>Volumen:</li>
+                </ul>
+                <ul className="column">
+                  <li>{year}</li>
+                  <li>{languaje}</li>
+                  <li>{pages}</li>
+                  <li>{vol}</li>
+                </ul>
               </div>
             </div>
           </div>
         </div>
       </div>
       <div className="Buttons">
-          <DropDownButtonDescarga />
-          <DropDownButtonCompartir />
+        <DropDownButtonDescarga />
+        <DropDownButtonCompartir />
 
         <Link to="/Report">
           <PrimaryBtnForm
