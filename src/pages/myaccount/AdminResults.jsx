ae58e6994acb79213261797d4f5c58afb47c9878
jfmonsa <juanmonsalve@protonmail.com>
2024-05-02 15:47:02 -0500

    dejando listo, para comenzar con la tabla

--- src/pages/myaccount/AdminResults.jsx
+++ src/pages/myaccount/AdminResults.jsx
@@ -1,8 +1,26 @@
 import Card from "../../components/card/Card";
 import Searcher from "../../components/searcher/Searcher";
 //Table imports
+// import { Paper, Table, TableBody, TableCell, TableContainer, TableHead, TablePagination, TableRow } from "@mui/material";
+import {useEffect, useState} from "react";
 
-const TableResults = () => {};
+const TableResults = () => {
+  const columns = [
+    {id: "id", name: "Id"},
+    {id: "name", name: "Name"},
+    {id: "email", name: "Email"},
+    {id: "phone", name: "Phone"},
+  ];
+
+  const handlechangepage = (event, newpage) => {
+    pagechange(newpage);
+  };
+  const handleRowsPerPage = event => {
+    rowperpagechange(+event.target.value);
+    pagechange(0);
+  };
+};
+
 const AdminResults = ({kindOfQuery}) => {
   return (
     <>
