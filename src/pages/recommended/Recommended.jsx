d1afea0c92f4bd44106ded38ca6c122863c22ae5
jfmonsa <juanmonsalve@protonmail.com>
2024-05-04 12:00:27 -0500

    Fixing some styles in recommend page

--- src/pages/recommended/Recommended.jsx
+++ src/pages/recommended/Recommended.jsx
@@ -2,43 +2,49 @@ import "./recommended.css";
 import Searcher from "../../components/searcher/Searcher";
 import Book from "../../components/book/Book";
 import image1 from "../../assets/img/image1.png";
+
+const GridBooks = ({books}) => {
+  return (
+    <>
+      <div className="gridBooks">
+        <Book title={"The Alchemist"} autor={"Pablo Coelho"} img={image1} />
+        <Book title={"The Alchemist"} autor={"Pablo Coelho"} img={image1} />
+        <Book title={"The Alchemist"} autor={"Pablo Coelho"} img={image1} />
+        <Book title={"The Alchemist"} autor={"Pablo Coelho"} img={image1} />
+
+        <Book title={"The Alchemist"} autor={"Pablo Coelho"} img={image1} />
+        <Book title={"The Alchemist"} autor={"Pablo Coelho"} img={image1} />
+        <Book title={"The Alchemist"} autor={"Pablo Coelho"} img={image1} />
+        <Book title={"The Alchemist"} autor={"Pablo Coelho"} img={image1} />
+
+        <Book title={"The Alchemist"} autor={"Pablo Coelho"} img={image1} />
+        <Book title={"The Alchemist"} autor={"Pablo Coelho"} img={image1} />
+        <Book title={"The Alchemist"} autor={"Pablo Coelho"} img={image1} />
+        <Book title={"The Alchemist"} autor={"Pablo Coelho"} img={image1} />
+
+        <Book title={"The Alchemist"} autor={"Pablo Coelho"} img={image1} />
+        <Book title={"The Alchemist"} autor={"Pablo Coelho"} img={image1} />
+        <Book title={"The Alchemist"} autor={"Pablo Coelho"} img={image1} />
+        <Book title={"The Alchemist"} autor={"Pablo Coelho"} img={image1} />
+      </div>
+      <div className="gridBooks__loadMoreContainer">
+        <button className="formCustomBtn gridBooks__loadMoreBtn baseBtn black2Btn">
+          Ver mas
+        </button>
+      </div>
+    </>
+  );
+};
+
 const Recommended = ({}) => {
   return (
     <>
-      <h1 className="title1">Recomendados</h1>
-      <h2 className="title2">Best ranked</h2>
-      <div>
-        <Searcher type={"text"} holder={"Buscar libro"} />
-      </div>
-      <div className="buttonMasOpciones">
-        <button className="buttonOpciones">Más opciones</button>
-      </div>
-      <div className="bookRecommended">
-        <Book title={"The Alchemist"} autor={"Pablo Coelho"} img={image1} />
-        <Book title={"The Alchemist"} autor={"Pablo Coelho"} img={image1} />
-        <Book title={"The Alchemist"} autor={"Pablo Coelho"} img={image1} />
-        <Book title={"The Alchemist"} autor={"Pablo Coelho"} img={image1} />
+      <h1 className="display--heading">Recomendados</h1>
+      <h2 className="display--subheading">Best ranked</h2>
 
-        <Book title={"The Alchemist"} autor={"Pablo Coelho"} img={image1} />
-        <Book title={"The Alchemist"} autor={"Pablo Coelho"} img={image1} />
-        <Book title={"The Alchemist"} autor={"Pablo Coelho"} img={image1} />
-        <Book title={"The Alchemist"} autor={"Pablo Coelho"} img={image1} />
-
-        <Book title={"The Alchemist"} autor={"Pablo Coelho"} img={image1} />
-        <Book title={"The Alchemist"} autor={"Pablo Coelho"} img={image1} />
-        <Book title={"The Alchemist"} autor={"Pablo Coelho"} img={image1} />
-        <Book title={"The Alchemist"} autor={"Pablo Coelho"} img={image1} />
-
-        <Book title={"The Alchemist"} autor={"Pablo Coelho"} img={image1} />
-        <Book title={"The Alchemist"} autor={"Pablo Coelho"} img={image1} />
-        <Book title={"The Alchemist"} autor={"Pablo Coelho"} img={image1} />
-        <Book title={"The Alchemist"} autor={"Pablo Coelho"} img={image1} />
-      </div>
-      <div className="ContainerVerMass">
-        <h1 className="line1"></h1>
-        <button className="buttonVerMas">Ver mas</button>
-        <h2 className="line2"></h2>
-      </div>
+      <Searcher type={"text"} holder={"Buscar libro"} />
+      {/* <button className="buttonOpciones">Más opciones</button> */}
+      <GridBooks />
     </>
   );
 };
