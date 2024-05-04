79963056922bbebbb5dc39249d063108cf4fe539
jfmonsa <juanmonsalve@protonmail.com>
2024-05-03 21:43:30 -0500

    table of results partially working

--- src/pages/myaccount/AdminResults.jsx
+++ src/pages/myaccount/AdminResults.jsx
@@ -1,11 +1,18 @@
 import Card from "../../components/card/Card";
 import Searcher from "../../components/searcher/Searcher";
 //Table imports
-// import { Paper, Table, TableBody, TableCell, TableContainer, TableHead, TablePagination, TableRow } from "@mui/material";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TablePagination,
+  TableRow,
+} from "@mui/material";
 import {useEffect, useState} from "react";
 import {
   getSearchUsers as getSearchUsersApi,
-  // createComment as createCommentApi,
   // deleteComment as deleteCommentApi,
   // updateComment as updateCommentApi,
 } from "../../data/apiSearchUsers.js";
@@ -26,25 +33,68 @@ const TableResults = () => {
     pagechange(0);
   };
 
-  const [rows, rowchange] = useState([]);
+  const [rows, setRowchange] = useState([]);
   const [page, pagechange] = useState(0);
   const [rowperpage, rowperpagechange] = useState(5);
 
   //Fetch data from api
   useEffect(() => {
-    getCommentsApi()
-      .then(resp => {
-        return resp.json();
-      })
-      .then(resp => {
-        rowchange(resp);
+    getSearchUsersApi()
+      .then(data => {
+        setRowchange(data);
+        console.log(data);
       })
       .catch(e => {
         console.log(e.message);
       });
   }, []);
 
-  return "hola";
+  return (
+    <>
+      {/* sx={{maxHeight: 450}} */}
+      <TableContainer>
+        <Table stickyHeader>
+          <TableHead>
+            <TableRow>
+              {columns.map(column => (
+                <TableCell
+                  // style={{backgroundColor: "black", color: "white"}}
+                  key={column.id}
+                >
+                  {column.name}
+                </TableCell>
+              ))}
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {rows &&
+              rows
+                .slice(page * rowperpage, page * rowperpage + rowperpage)
+                .map((row, i) => {
+                  return (
+                    <TableRow key={i}>
+                      {columns &&
+                        columns.map((column, i) => {
+                          let value = row[column.id];
+                          return <TableCell key={value}>{value}</TableCell>;
+                        })}
+                    </TableRow>
+                  );
+                })}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <TablePagination
+        rowsPerPageOptions={[5, 10, 25]}
+        rowsPerPage={rowperpage}
+        page={page}
+        count={rows.length}
+        component="div"
+        onPageChange={handlechangepage}
+        onRowsPerPageChange={handleRowsPerPage}
+      ></TablePagination>
+    </>
+  );
 };
 
 const AdminResults = ({kindOfQuery}) => {
