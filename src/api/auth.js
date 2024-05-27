0b17a810406a9b3736dfcc8f05d76e14467d9dd3
Juan Pablo Idarraga <94719824+JuanPidarraga@users.noreply.github.com>
2024-05-26 23:37:31 -0500

    Update auth.js

--- src/api/auth.js
+++ src/api/auth.js
@@ -4,10 +4,5 @@ export const registerRequest = user => axios.post(`/register`, user);
 
 export const loginRequest = user => axios.post(`/login`, user);
 
-export const verifyTokenRequest = () => axios.get(`/verifyToken`);
-
-feature/search_filter
-
-
 export const verifyTokenEmail = token => axios.post(`/verifyTokenEmail`, token);
-main
+
