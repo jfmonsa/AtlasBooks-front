3ffbaca5a8464a75227ed44f7112a47cdd575554
NicolasPL <31411531+NicolasPL64@users.noreply.github.com>
2024-11-06 21:25:46 -0500

    Updated base URL

--- src/api/baseUrl.js
+++ src/api/baseUrl.js
@@ -2,7 +2,7 @@ const typeBackend = "productionn";
 
 const baseUrl =
   typeBackend === "production"
-    ? "https://atlas-books-back.vercel.app/api"
-    : "http://localhost:3000/api";
+    ? "https://atlas-books-back.vercel.app/api/v1"
+    : "http://localhost:3000/api/v1";
 
 export default baseUrl;
