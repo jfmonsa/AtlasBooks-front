8b0b183f96806d107c80f820196db3bcaaed09fa
NicolasPL <31411531+NicolasPL64@users.noreply.github.com>
2024-11-07 23:05:02 -0500

    Updated logout and verifyToken

--- src/api/auth.js
+++ src/api/auth.js
@@ -5,7 +5,9 @@ export const registerRequest = user => axios.post(`/auth/register`, user);
 
 export const loginRequest = user => axios.post(`/auth/login`, user);
 
-export const verifyTokenRequest = () => axios.get(`/auth/verifyToken`);
+export const logoutRequest = () => axios.post(`/auth/logout`);
+
+export const verifyTokenRequest = () => axios.post(`/auth/verifyToken`);
 
 export const verifyTokenEmail = token => axios.post(`/auth/verifyTokenEmail`, token);
 // TODO: This endpoint changed to (probably) /auth/change-email-confirmed
